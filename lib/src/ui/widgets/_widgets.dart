022f21d87925202e383a11cc90eeac7974fbdb7f
emilakerman <emilaakerman@gmail.com>
2024-01-30 16:49:23 +0100

    added more barrel files

--- lib/src/ui/widgets/_widgets.dart
+++ lib/src/ui/widgets/_widgets.dart
@@ -2,4 +2,4 @@ export 'cast_alert.dart';
 export 'horizontal_categories.dart';
 export 'show_dialog.dart';
 export 'youtube_info_widget.dart';
-export 'youtube_short.dart';
+export '../../core/models/youtube_short.dart';
